98492600d839c3ad16ac204676d305b4c9efb922
ulfunhy <myfamily11141@gmail.com>
2024-10-14 12:37:49 +0900

    ID, Password 입력받은 후 다른 Activity로 이동 및 출력

--- app/src/main/java/com/ulfunhy/clientproject/LoginActivity.java
+++ app/src/main/java/com/ulfunhy/clientproject/LoginActivity.java
@@ -1,15 +1,30 @@
 package com.ulfunhy.clientproject;
 
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
 
+    TextView ID, Password;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         setContentView(R.layout.activity_login);
+
+        ID = findViewById(R.id.ID);
+
+        Intent loginIntent = getIntent();
+
+        String id = loginIntent.getStringExtra("ID");
+
+        ID.setText(id);
     }
+
 }
--- app/src/main/java/com/ulfunhy/clientproject/MainActivity.java
+++ app/src/main/java/com/ulfunhy/clientproject/MainActivity.java
@@ -1,27 +1,42 @@
 package com.ulfunhy.clientproject;
 
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button loginBtn;
+    EditText LoginID, LoginPw;
+    Button LoginBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         setContentView(R.layout.activity_main);
 
-        loginBtn = findViewById(R.id.loginBtn);
-        loginBtn.setOnClickListener(new View.OnClickListener() {
+        LoginID = findViewById(R.id.LoginID);
+        LoginPw = findViewById(R.id.LoginPw);
+
+        LoginBtn = findViewById(R.id.LoginBtn);
+
+        LoginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-                startActivity(intent);
+                Intent mainIntent = new Intent(MainActivity.this, LoginActivity.class);
+                String loginID = MainActivity.this.LoginID.getText().toString();
+                String loginPw = MainActivity.this.LoginPw.getText().toString();
+                mainIntent.putExtra("ID",loginID);
+                mainIntent.putExtra("Password",loginPw);
+
+
+                startActivity(mainIntent);
             }
         });
     }
